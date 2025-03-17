11b3ae03784607952154ad10de9b7b6641b741cc
nerm1k <nerm1k@mail.ru>
2025-03-17 10:35:57 +0500

    setup

---
+++ front-vkr/src/App.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { Provider } from 'react-redux'
+import { store } from './store/store'
+import { Routes } from 'react-router-dom'
+
+function App() {
+
+  return (
+    <>
+      <Provider store={store}>
+        <Routes>
+          {/* <Route path="/" element={<Layout />}>
+            <Route path="/" element={<MainPage />} />
+          </Route> */}
+          {/* <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/logout" element={<LogoutPage />} /> */}
+        </Routes>
+      </Provider>
+    </>
+  )
+}
+
+export default App
